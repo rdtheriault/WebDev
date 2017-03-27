8b52d404c7ec0b65e9fd6b81162b8a7e34b64d30
rdtheriault <rdtheriault@yahoo.com>
2017-03-27 09:03:31 -0700

    Automatic Backup @ Mon Mar 27 09:03:31 PDT 2017

---
+++ html/classes/hhs/hhs_blog/insert.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+  
+  </head>
+  <body>
+    <?php
+
+
+		$s3 = "Insert into hhs_blog (user, category, info, title) Values ('".$name."','".$category."','".$info."-".$title."')";
+		
+		$q3 = mysqli_query($dbc,$s3);
+		if($q3)
+		{
+		   	echo '<br>Your work sample has been archived'.$counter; 
+		   	$counter = $counter + 1;
+		   	$_SESSION['counter'] = $counter;
+		}
+		else
+		{
+			echo '<p>'.mysqli_error($dbc).'</p>';
+			echo 'Archive Query issue';
+		}
+
+	?>
+    
+    
+  </body>
+</html>
